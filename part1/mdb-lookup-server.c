0a53536b071d3e3afd515393b8419637dabad411
Steven Liu <xl2948@columbia.edu>
2020-04-11 12:17:08 -0400

    I think I finished part 1

--- part1/mdb-lookup-server.c
+++ part1/mdb-lookup-server.c
@@ -49,6 +49,36 @@ void setEnd(struct List *list){
     }
 }
 
+void findData2(char *string, struct List *list, int sock){
+    char *output = calloc(68, 1);
+	struct Node *pointer = list -> head;
+	int counter = 1;
+	while (pointer != NULL){
+		struct MdbRec *data = pointer -> data;
+		//printf("%d%d ", strstr(data -> name, string) != NULL, strstr(data -> msg, string) != NULL);
+		//printf("%s %s %s \n", data->name, data->msg, string);
+		if (strstr(data -> name, string) != NULL||strstr(data -> msg, string) != NULL){
+			if (counter < 10) {
+                snprintf(output, 68, "   %d: {%s} said {%s} \n", counter, data -> name, data -> msg);
+                send(sock, output, strlen(output), 0);
+            }
+            if (counter < 100 && counter >=10) {
+                snprintf(output, 68, "  %d: {%s} said {%s} \n", counter, data -> name, data -> msg);
+                send(sock, output, strlen(output), 0);
+            }
+			if (counter >= 100) {
+                snprintf(output, 68, " %d: {%s} said {%s} \n", counter, data -> name, data -> msg);
+                send(sock, output, strlen(output), 0);
+            }
+            //snprintf(*output, 64, "%d: {%s} said {%s} \n", counter, data -> name, data -> msg);
+		}
+		counter++;
+		pointer = pointer -> next;
+	}
+    free(output);
+}
+
+
 void findData(char *string, struct List *list){
 	struct Node *pointer = list -> head;
 	int counter = 1;
@@ -66,7 +96,6 @@ void findData(char *string, struct List *list){
 		pointer = pointer -> next;
 	}
 }
-
 void freeData(struct List *list){
 	struct Node *pointer = list -> head;
 	while (pointer != NULL){
@@ -95,9 +124,6 @@ int main(int argc, char **argv)
     // Open file
     
     char *filename = argv[1];
-    FILE *fp = fopen(filename, "rb");
-    if (fp == NULL) 
-        die(filename);
 
     // Port number
 
@@ -134,12 +160,12 @@ int main(int argc, char **argv)
     struct sockaddr_in clntaddr;
     
     
-    int r;
-    char buf[4096];
+    //int r;
+    //char buf[4096];
     //uint32_t size, size_net, remaining, limit;
     //struct stat st;
 
-    //while (1) {
+    while (1) {
 
         // Accept an incoming connection
 
@@ -148,13 +174,19 @@ int main(int argc, char **argv)
         if ((clntsock = accept(servsock, (struct sockaddr *) &clntaddr, &clntlen)) < 0)
             die("accept failed");
 
-        // Print ip address
+        // Print starting message
 
-        fprintf(stderr, "client ip: %s\n", inet_ntoa(clntaddr.sin_addr));
+        fprintf(stderr, "connection started from: %s\n", inet_ntoa(clntaddr.sin_addr));
 
         // Process socket descriptor
 
         FILE *input = fdopen(clntsock, "r");
+
+        // Open file
+
+        FILE *fp = fopen(filename, "rb");
+        if (fp == NULL) 
+            die(filename);
 
         //Create & Initialize a list
 
@@ -163,19 +195,14 @@ int main(int argc, char **argv)
     
     	//Read data into the list
 
-
-       while (1){
+        while (1){
     	    struct MdbRec *buffer = malloc(40);
 	    	if (fread(buffer, 40, 1, fp) == 1){
-	    		//printf("%s ", buffer -> name);
-	            //printf("%s \n", buffer -> msg);
 	    		appendNode(&list, buffer);
 	    	}else{
 	    		free(buffer);
-                printf("finished reading\n");
 	    		break;
 	    	}
-	    	//free(buffer);
         }
 	
 	    //Set the end of every string '\n'
@@ -183,35 +210,42 @@ int main(int argc, char **argv)
 	    setEnd(&list);
     
         //r = recv(clntsock, buf, 5, 0);
+        char *in = calloc(1000, 1);
+
+        while (fgets(in, 1000, input)){
         char *proc = calloc(6, 1);
-        fgets(proc, 6, input);
+            strncpy(proc, in, 5);
+		    for(int i=0; i<6; i++){
+		    	if(!isprint(proc[i])){
+		    		proc[i] = '\0';
+		    	}
+		    }
+            //char *out = calloc(1000, 1);
+            //findData(proc, &list);
+            findData2(proc, &list, clntsock);
+            free(proc);
+            send(clntsock, "\n", 1, 0);
+        }  
+
         fclose(input);
-        
-		//strncpy(proc, buf, 5);
-        
-		for(int i=0; i<6; i++){
-			if(!isprint(proc[i])){
-				proc[i] = '\0';
-			}
-		}
-		
-        //printf("print: %s", proc);
-        
-		findData(proc, &list);
-        free(proc);
-        
+        fclose(fp);
+        free(in);
+        //printf("%s", output);
 
         // Finally, close the client connection and go back to accept()
 
         close(clntsock);
-        printf("Loop Ended");
 
         //Free everything
         freeData(&list);
         while ((popFront(&list)) != NULL) {
             ;
         }
-    //}
+
+        //Print termination message
+        
+        fprintf(stderr, "connection terminated from: %s\n", inet_ntoa(clntaddr.sin_addr));
+    }
 
     
 }
