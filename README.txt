133967d39ba5fb6d5d90864e5d80ab0d04984ee3
Steven Liu <xl2948@columbia.edu>
2020-04-12 22:12:02 -0400

    I think I finished. LOL

--- README.txt
+++ README.txt
@@ -6,3 +6,5 @@
 Works as required by the lab handout
 
 2)
+Works as required by the lab handout, run "make vtest" to run valgrind
+example. There should be no memory errors.
--- part2/http-client.c
+++ part2/http-client.c
@@ -8,6 +8,8 @@
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <ctype.h>
 
 static void die(const char *s) { perror(s); exit(1); }
 
@@ -19,10 +21,26 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    const char *ip = argv[1];
+	// Format the IP
+	
+	struct hostent *he;
+    char *serverName = argv[1];
+    // get server ip from server name
+    if ((he = gethostbyname(serverName)) == NULL) {
+        die("gethostbyname failed");
+    }
+	char *ip = inet_ntoa(*(struct in_addr *)he->h_addr);
+
+	// Take in port number and file path
+	
     unsigned short port = atoi(argv[2]); // ARRAY TO INTEGER
     const char *filepath = argv[3];
-
+	char *filename = strrchr(filepath, '/') + 1;
+	//printf("File name: %s\n", filename);
+	if (filename[0] == '\0'){
+		printf("can't open output file: No such file or directory\n");
+		exit(1);
+	}
     // Create a socket for TCP connection
 
     int sock; // socket descriptor
@@ -41,21 +59,75 @@ int main(int argc, char **argv)
 
     if (connect(sock, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
         die("connect failed");
-	//else printf("success\n");
 
-    //Send stuff
+    // Send stuff
 	
 	char request[1000];
-	sprintf(request, "GET %s HTTP/1.0\r\nHost: %s:<%d>\r\n\r\n", filepath, ip, port);
-	printf("--------\n%s--------\n", request);
+	sprintf(request, "GET %s HTTP/1.0\r\nHost: %s:%d\r\n\r\n", filepath, serverName, port);
+	
+	//printf("%s--------\n", request);
     send(sock, request, strlen(request), 0);
-    char buff[4096];
-	recv(sock, buff, 2000, MSG_WAITALL);
-	printf("%s", buff);
-	
-	
-	
 
+	// Receive stuff
+    //char buff[4096];
+	
+	FILE *response = fdopen(sock, "rb");
+
+	// Receive first line
+	char c;
+	char firstLine[20];
+	int counter = 0;
+	while ((c = fgetc(response))!='\r'){
+		firstLine[counter] = c;
+		counter++;
+	}
+	firstLine[counter] = 0;
+
+	if (firstLine[9] == '2' && firstLine[10] == '0' && firstLine[11] == '0'){
+			//printf("Success");
+	}else{
+			printf("%s\n", firstLine);
+			die("");
+	}
+	
+	// Look for the blank line
+	
+	char last = '\r';
+	while(1){
+		c = fgetc(response);
+		if ((last == '\n') && (c == '\r')){
+			//printf("Found");
+			break;
+		}
+		last = c;
+	}
+	c = fgetc(response);
+
+	// Read the content into the file
+	
+	FILE *fp = fopen(filename, "wb");
+	
+	int i = 0;
+	char buffer[4096];
+	while (1){
+		//printf("%d", i);
+		i = fread(buffer, 1, 4096, response);
+		fwrite(buffer, 1, i, fp);
+		if (i < 4096) {break;}
+	}
+	
+	/*
+	while ((c = fgetc(response)) != EOF){
+		//printf("%c", c);
+		i++;
+		fputc(c, fp);
+	}
+	*/	
+	printf("%d", i);
+
+	fclose(fp);
+	fclose(response);
+	
 	// Clean-up
 
     close(sock);
